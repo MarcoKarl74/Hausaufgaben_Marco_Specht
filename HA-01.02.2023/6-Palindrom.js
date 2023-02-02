85f914a8d37549d191ac408383073b5d344ae5bf
MarcoKarl74 <115979532+MarcoKarl74@users.noreply.github.com>
2023-02-02 17:08:01 +0100

    Add files via upload

---
+++ HA-01.02.2023/6-Palindrom.js
@@ -0,0 +1,10 @@
+// 6. Schreiben Sie ein JavaScript-Programm, um herauszufinden, 
+// ob ein Wort ein Palindrom ist, oder nicht. (z. B. Madam, Anna, Otto)
+
+
+function isPalindrome(word) {
+    word = word.toLowerCase();
+    return word === word.split('').reverse().join('');
+  }
+  
+  console.log(isPalindrome("Reliefpfeiler")); // true
