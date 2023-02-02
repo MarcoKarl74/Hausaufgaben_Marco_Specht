2096cd8cedd33f42207c371043027c874f0aa7ab
MarcoKarl74 <115979532+MarcoKarl74@users.noreply.github.com>
2023-02-02 16:03:23 +0100

    Add files via upload

--- HA-01.02.2023/1-DreieckFläche.js
+++ HA-01.02.2023/1-DreieckFläche.js
@@ -1,1 +1,12 @@
+// Schreiben Sie ein JavaScript-Programm, um den Flächeninhalt eines Dreiecks 
+// zu ermitteln, dessen drei Seitenlängen a, b, c sind
 
+
+function triangleArea(a, b, c) {
+    let s = (a + b + c) / 2;
+    let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    console.log(area);
+    return area;
+  }
+
+triangleArea(8, 12,5);
---
+++ HA-01.02.2023/2-TechstarterInterval.js
@@ -0,0 +1,10 @@
+// 2. Schreiben Sie ein JavaScript-Programm, um den String 'Techstarter' 
+// periodisch alle 2 Sekunden auf die Konsole anzuzeigen.
+
+
+
+
+setInterval(function() {
+  console.log('Techstarter');
+}, 2000);
+
